9045e55755adc58efd77f3cb51796b4e3f92fc3c
bonecrushereb <bonecrushereb@gmail.com>
2016-02-11 23:51:33 -0800

    fixed linked type issue

--- app.js
+++ app.js
@@ -1,77 +1,77 @@
 // // JS file for Feb 9 class demo
 
-// var userName = prompt('What is your name?');
-// alert('Welcome ' + userName + ', can you guess some stuff about me correctly?');
-// console.log('The user said their name is ' + userName);
-//
-// var ready = prompt('Are you ready to play the game? Don\'t forget to answer with a \'Y\' or \'N\' ');
-// if(ready.toUpperCase() === 'Y' || ready.toUpperCase() === 'YES'){
-//   alert('Great lets continue!');
-// }else if (ready.toUpperCase() === 'N' || ready.toUpperCase() === 'NO') {
-//   alert('that\'s to bad.');
-// }else {
-//   alert('You typed in a bad response');
-// }
-// console.log('The user said ' + ready);
-//
-// var born = prompt('Do you think I was born in San Diego, California?');
-// if(born.toUpperCase() === 'Y' || born.toUpperCase() === 'YES'){
-//   alert('That answer is incorrect. I was actually born in Denver, Colorado. I did however move to San Diego, California');
-// }else if(born.toUpperCase() === 'N' || born.toUpperCase() === 'NO'){
-//   alert('Wow you know me well!');
-// }else {
-//   alert('You typed in a bad response!');
-// }
-// console.log('The user guessed where I was born' + born );
-//
-// var degree = prompt('Do I have a BS degree?');
-// if(degree.toUpperCase() === 'Y' || degree.toUpperCase() === 'YES'){
-//   alert('That is correct, I recieved my degree from the Art Institute of San Diego, California');
-// }else if(degree.toUpperCase() === 'N' || degree.toUpperCase() === 'NO'){
-//   alert('Sorry that answer is wrong');
-// }else{
-//   alert('You typed in a bad response!')
-// }
-// console.log('The user gussed ' + degree + ' about my degree');
-//
-// var work = prompt('Did I work at naughtyDog?');
-// if(work.toUpperCase() === 'Y' || work.toUpperCase() === 'YES'){
-//   alert('That is incorrect, sorry');
-// }else if(work.toUpperCase() === 'N' || work.toUpperCase() === 'NO'){
-//   alert('That is correct, I worked on Evequest for 2 years!');
-// }else{
-//   alert('You typed in a bad response');
-// }
-//
-// var goal = prompt('Is my end goal to become a succesful developer');
-// if(goal.toUpperCase() === 'Y' || goal.toUpperCase() === 'YES'){
-//   alert('That is correct, doesn\'t everyone!');
-// }else if (goal.toUpperCase() === 'N' || goal.toUpperCase() === 'NO') {
-//   alert('That is incorrect!');
-// }else{
-//   alert('You typed in a bad response');
-// }
-//
-// while(age != 26){
-//   var age = prompt('How old do you think I am?');
-//   if(age > 26){
-//     alert('You are to high!!!');
-//   }else if(age < 26){
-//     alert('You are to low!!');
-//   }
-// }
-// alert('You are correct!')
-// console.log('The user typed ' + age + 'for my age');
-//
-// var pets = ['Finn','Amara'];
-// var petAnswer = prompt('Can you name one of Ben\'s pets?');
-// for (var i = 0; i < pets.length; i++) {
-//   console.log(pets[i]);
-//   if(petAnswer === pets[i]){
-//     alert('YES');
-//   }
-// }
-// alert('You had 2 guesses and failed!');
+var userName = prompt('What is your name?');
+alert('Welcome ' + userName + ', can you guess some stuff about me correctly?');
+console.log('The user said their name is ' + userName);
+
+var ready = prompt('Are you ready to play the game? Don\'t forget to answer with a \'Y\' or \'N\' ');
+if(ready.toUpperCase() === 'Y' || ready.toUpperCase() === 'YES'){
+  alert('Great lets continue!');
+}else if (ready.toUpperCase() === 'N' || ready.toUpperCase() === 'NO') {
+  alert('that\'s to bad.');
+}else {
+  alert('You typed in a bad response');
+}
+console.log('The user said ' + ready);
+
+var born = prompt('Do you think I was born in San Diego, California?');
+if(born.toUpperCase() === 'Y' || born.toUpperCase() === 'YES'){
+  alert('That answer is incorrect. I was actually born in Denver, Colorado. I did however move to San Diego, California');
+}else if(born.toUpperCase() === 'N' || born.toUpperCase() === 'NO'){
+  alert('Wow you know me well!');
+}else {
+  alert('You typed in a bad response!');
+}
+console.log('The user guessed where I was born' + born );
+
+var degree = prompt('Do I have a BS degree?');
+if(degree.toUpperCase() === 'Y' || degree.toUpperCase() === 'YES'){
+  alert('That is correct, I recieved my degree from the Art Institute of San Diego, California');
+}else if(degree.toUpperCase() === 'N' || degree.toUpperCase() === 'NO'){
+  alert('Sorry that answer is wrong');
+}else{
+  alert('You typed in a bad response!')
+}
+console.log('The user gussed ' + degree + ' about my degree');
+
+var work = prompt('Did I work at naughtyDog?');
+if(work.toUpperCase() === 'Y' || work.toUpperCase() === 'YES'){
+  alert('That is incorrect, sorry');
+}else if(work.toUpperCase() === 'N' || work.toUpperCase() === 'NO'){
+  alert('That is correct, I worked on Evequest for 2 years!');
+}else{
+  alert('You typed in a bad response');
+}
+
+var goal = prompt('Is my end goal to become a succesful developer');
+if(goal.toUpperCase() === 'Y' || goal.toUpperCase() === 'YES'){
+  alert('That is correct, doesn\'t everyone!');
+}else if (goal.toUpperCase() === 'N' || goal.toUpperCase() === 'NO') {
+  alert('That is incorrect!');
+}else{
+  alert('You typed in a bad response');
+}
+
+while(age != 26){
+  var age = prompt('How old do you think I am?');
+  if(age > 26){
+    alert('You are to high!!!');
+  }else if(age < 26){
+    alert('You are to low!!');
+  }
+}
+alert('You are correct!')
+console.log('The user typed ' + age + 'for my age');
+
+var pets = ['Finn','Amara'];
+var petAnswer = prompt('Can you name one of Ben\'s pets?');
+for (var i = 0; i < pets.length; i++) {
+  console.log(pets[i]);
+  if(petAnswer === pets[i]){
+    alert('YES');
+  }
+}
+alert('You had 2 guesses and failed!');
 
 var ctr = 4;
 console.log('Hello madude ' + (ctr > 0 ))
@@ -91,4 +91,25 @@ while(favNum != 20 && ctr > 0 ){
   }else{
     alert('You are correct!!')
   }
+}
+
+var lived = prompt('Can you guess where I have lived?');
+var states = ['Colorado', 'Ohio', 'New Mexico', 'California', 'Washington'];
+// var correctGuesses = [];
+var isCorrect = false;
+
+
+for (i = 0; i < states.length; i++){
+  if(lived === states[i]){
+    // correctGuesses.push(lived);
+    isCorrect = true;
+    // alert(correctGuesses[i] + ' are Correct!');
+    // console.log('The user entered ' + correctGuesses);
+  }
+}
+
+if (isCorrect == false) {
+  alert('you were wrong!');
+} else {
+  alert('hurray you got it right!');
 }
